199596e5d21e9c673be8cdeee8324692b74032ff
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-07-20 17:26:16 +0200

    added tezos ico

--- src/config.js
+++ src/config.js
@@ -5,6 +5,48 @@ const rpcHost = require('./env.json').rpcHost;
 
 export default {
   ICOs: {
+    '0xb56d622DDF60ec532B5f43B4Ff9B0e7b1FF92dB3': {
+      information: {
+        aliasName: 'TEZOS Fundraiser',
+        logo: 'https://www.tezos.com/static/favicon.ico',
+        website: 'https://www.tezos.com/',
+      },
+      event: {
+        args: {
+          tokens: null, // not an ICO
+          sender: 'tezos_pk_hash',
+        },
+        name: 'Deposit',
+        firstTransactionBlockNumber: 0,
+        lastTransactionBlockNumber: "latest"
+      },
+      icoParameters: {
+        cap: async(icoContract) => "unbounded",
+        startDate: async(icoContract) => "NOT AND ICO!",
+        endDate: async(icoContract) => "NOT AND ICO!",
+        status: async(icoContract) => {
+          const isRunning = await toPromise(icoContract.accept)();
+          // tezos does what they want. may start at any moment in the future
+          return isRunning.valueOf() ? 'in progress' : 'successful';
+        }
+      },
+      matrix: {
+        q1: { answer: true},
+        q2: { answer: true},
+        q3: { answer: true, comment: "They didn't bother to attach code to actual fundraising smart contract but etherscan is solving this by bytecode search"},
+        q4: { answer: true},
+        q5: { answer: false, comment: 'They do not track senders of ETH, no refund mechanism'},
+        q6: { answer: true},
+        q7: { answer: true},
+        q8: { answer: null},
+        q9: { answer: null},
+        q10: { answer: true},
+        q11: {answer: false, comment: 'No investor rights are protected. You send money and Tezos takes it. Not any better than sending $$$ in envelope to Tezos office.'},
+        q12: { answer: false, comment: 'Not an ICO - no tokens created' },
+        q13: { answer: false, comment: 'May be started and re-started whenever Tezos wants'},
+        q14: { answer: false, comment: 'May be stopped and re-started whenever Tezos wants'},
+      },
+    },
     '0xF8094e15c897518B5Ac5287d7070cA5850eFc6ff': {
       tokenContract: '0x0abdace70d3790235af448c88547603b945604ea',
       information: {
@@ -301,7 +343,6 @@ export default {
       },
     },
   },
-    // https://neufund.net/nodes/mainnet
   rpcHost,
   defaultDecimal: 18,
   matrix: {

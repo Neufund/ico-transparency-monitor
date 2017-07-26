4f01227734e79bb316a565963c57c299b4df0990
mostafabalata <mostafa.balata@gmail.com>
2017-07-26 16:30:31 +0200

    get smart contract

--- tests/utils/utils.js
+++ tests/utils/utils.js
@@ -1,12 +1,19 @@
 import { assert, expect, should } from 'chai';
 import {
-    computeICOTransparency,
-    getEtherRate,
-    getICOs,
-    getValueOrNotAvailable,
-    getStatistics,
-    initStatistics,
-    getICOLogs } from '../../src/utils';
+  getSmartContract } from '../../src/utils/web3';
 
 import config from '../../src/config';
 
+describe('getSmartContract', () => {
+  it('should return web3 smart contract', async () => {
+    const dispatchMock = spy();
+    const stateProvider = () => ({
+      modal: {
+        web3: {}
+      }
+    });
+
+    getSmartContract(web3, '0xa74476443119a942de498590fe1f2454d7d4ac0d')
+
+  });
+});

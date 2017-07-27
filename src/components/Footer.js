42feb28dcd71f87b4f784d67092356c915af633c
mostafabalata <mostafa.balata@gmail.com>
2017-07-27 18:41:17 +0200

    adding footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+export default() => (
+  <Grid fluid className="footer">
+    <Row>
+      <Col md={12}>
+      If you want to give us feedback write us at icomonitor@neufund.org
+      </Col>
+    </Row>
+  </Grid>
+)
--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TopHeader from '../components/TopHeader';
+import Footer from '../components/Footer';
 
 const Layout = ({ ...props }) => (<div>
   <TopHeader />
   <div>
     {props.children}
   </div>
+  <Footer />
 </div>);
 
 export default Layout;

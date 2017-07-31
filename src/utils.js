93f2ec736f4fcc82d2e9b1a3e7e21c162cd05f4f
rudolfix <marcin.rudolf@neufund.org>
2017-07-31 13:38:19 +0200

    use ordered gini to decrease from o(n^2) to o(n) (#53)
    * use ordered gini to decrease from o(n^2) to o(n)
    
    * removed comments

--- src/utils.js
+++ src/utils.js
@@ -428,10 +428,11 @@ export const getStatistics = (icoConfig, allLogs, stats) => {
   );
 
   if (stats.money.tokenIssued > 0) {
-    const tokens = Object.keys(stats.investors.senders)
-      .map(investor => stats.investors.senders[investor].tokens);
-    stats.general.giniIndex = gini.unordered(tokens);
+    const tokens = sortedSenders[0].map(investor => investor.value);
+    // todo: should just rewrite gini.ordered to accept reverse ordered array
+    stats.general.giniIndex = gini.ordered(tokens.reverse());
   }
 
+  console.log('dictionaries completed');
   return [stats, csvContentArray];
 };

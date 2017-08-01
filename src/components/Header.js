1d70940ebc2c70c3a2e4028d51e180787fcbde49
mostafabalata <mostafa.balata@gmail.com>
2017-08-01 19:25:32 +0200

    add legal data

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,23 @@ const Header = () => (
             <p>A go to place for investors and journalists to learn whether a given ICO is transparent and
                             can be trusted. The ICO Monitor serves as a transparency benchmark for the community to
                             better evaluate offerings and make informed decisions based on objective metrics.</p>
+            <p>
+              <h4>Transparency Legends explaining</h4>
+
+              <ul>
+              <li>Non-transparent: this rating means that one or more essential dimensions of trust and security receive an unfavorable rating.</li>
+              <li>Transparent with issues: this rating means that one or more non-essential dimensions of trust and security receive an unfavorable rating.</li>
+              <li>Transparent: this rating means that all dimension of trust and security receive a favorable rating.</li>
+            </ul>
+
+              If you want read more about this legents click <a href="https://github.com/Neufund/ico-transparency-monitor#transparency-decision" target="_blank">here</a>.
+            </p>
+
+              <p>
+                ICO Monitor is an open platform. We take no responsibility for the information published or data provided.
+                We also do not assume any liability of their correctness.
+              </p>
+
             <a href="https://github.com/Neufund/ico-transparency-monitor#adding-custom-icos-to-the-transparency-monitor" target="_blank" className="add-new-ico">
               <i className="fa fa-plus" aria-hidden="true" />Submit New ICO or Update HERE! for free</a>
           </Col>

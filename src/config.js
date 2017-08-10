9a0ff8476f166795ac13a45d3b9fa9846b34053d
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-08-10 12:51:57 +0200

    fixes typos, multiline string

--- src/config.js
+++ src/config.js
@@ -92,16 +92,16 @@ let config = {
       },
       matrix: {
         q1: { answer: false, comment:
-          "Funds in ETH, BTC and USD are gathered by Coinlist's backend so Ether is processed no different form fiat. "
-          + "HD wallets are created for participants (BIP32 we assume) which are probably monitored by backend that can derive priv keys to control funds. "
-          + "CoinList could provide public master key for ETH and BTC to let external auditors monitor ICO funds, but we cannot find those."},
+          `Funds in ETH, BTC and USD are gathered by Coinlist's backend so Ether is processed no different form fiat. 
+           HD wallets are created for participants (BIP32 we assume) which are probably monitored by backend that can derive private keys to control funds. 
+          CoinList could provide public master key for ETH and BTC to let external auditors monitor ICO funds, but we cannot find those.`},
         q2: { answer: false },
         q3: { answer: false },
         q4: { answer: null },
         q5: { answer: null },
-        q6: { answer: false, comment: "There is no way to externally monitor ETH funds as master public key is not provided. You are as good as sending a signed transaction in envelope to coinlist."  },
+        q6: { answer: false, comment: "There is no way to externally monitor ETH funds as master public key is not provided. You are as good as sending a signed transaction in envelope to Coinlist."  },
         q7: { answer: false},
-        q8: { answer: false, comment: "Fiat is entirelly handled by banks, no pegged token of any kind is used, that eliminates fiat from any kind of auditing." },
+        q8: { answer: false, comment: "Fiat is entirely handled by banks, no pegged token is used to represent it, that eliminates fiat from any external auditing." },
         q9: { answer: false},
         q10: { answer: null },
         q11: { answer: null },

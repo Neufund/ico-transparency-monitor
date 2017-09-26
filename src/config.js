576d0e0af1ab5baa25419b1ad60e706d3f672d41
Marcin Rudolf <marcin.rudolf@neufund.org>
2017-09-26 16:42:25 +0200

    fixes CHEX ico config

--- src/config.js
+++ src/config.js
@@ -241,9 +241,73 @@ const filecoin = {
   addedBy: 'Mostafa Balata',
 };
 
+let chex = {
+  information: {
+    aliasName: 'CHEX',
+    logo: 'https://tokensale.thechex.com/images/chex-token.jpg',
+    website: 'https://tokensale.thechex.com',
+  },
+  events: {
+    Transfer: {
+      args: {
+        tokens: 'value',
+        sender: 'to',
+      },
+      customArgs: {
+        _from: '0x0000000000000000000000000000000000000000',
+      },
+      firstTransactionBlockNumber: 4203059,
+      lastTransactionBlockNumber: "latest",
+      countTransactions: true,
+    }
+  },
+  icoParameters: {
+    cap: async(web3, icoContract) => {
+      const total = convertWeb3Value(await toPromise(icoContract.tokenCap)(), 'ether');
+      return `Max ${formatNumber(total)} CHX`;
+    },
+    startDate: async icoContract => 'contract creation',
+    endDate: async icoContract => 'until cap reached',
+    status: async(web3, icoContract) => {
+      const frozen = await toPromise(icoContract.frozen)().valueOf();
+      const maxCap = convertWeb3Value(await toPromise(icoContract.tokenCap)(), 'ether');
+      const supply = convertWeb3Value(await toPromise(icoContract.totalSupply)(), 'ether');
+      return maxCap === supply ? 'successful' :
+        frozen ? 'paused' : 'in progress';
+    },
+  },
+  matrix: {
+    q1: {answer: true},
+    q2: {answer: true},
+    q3: {answer: true},
+    q4: {answer: true},
+    q5: {answer: true},
+    q6: {answer: true},
+    q7: {answer: true},
+    q8: {
+      answer: true,
+      comment: 'Adjusts to estimate current bitcoin:ethereum prices, and generates a receipt for real time transaction'
+    },
+    q9: {
+      answer: false,
+      comment: 'Non-ethereum transactions (bitcoin, altcoins) are processed primarily through a centralized server'
+    },
+    q10: {answer: true, comment: 'Simplified and uses common practices'},
+    q11: {
+      answer: true,
+      comment: 'The only exception to this is that the contract uses block time to change phases, which can be lead to slightly early/late dates depending on network congestion'
+    },
+    q12: {answer: true, comment: 'Price scales according to current block number while sale is active'},
+    q13: {answer: true, comment: 'Determined by starting block number'},
+    q14: {answer: true, comment: 'Will end either by supply limit reached, manual freeze, or end block reached'},
+  },
+  addedBy: 'Bravetarget',
+};
+
 let config = {
   ICOs: {
     'kin-smart-contract-not-provided': kin,
+    '0xD566Fa4a696EAc66f749f7fe999D6673fEe2026c': chex,
     '0xd4FD252d7D2C9479a8d616F510eAC6243B5DDdf9': zrx,
     'filescoin-smart-contract-not-provided': filecoin,
     '0x1d0dcc8d8bcafa8e8502beaeef6cbd49d3affcdc': gnosis,
@@ -773,56 +837,6 @@ let config = {
         q14: {answer: null},
       },
       addedBy: 'Mostafa Balata',
-    },
-    '0xD566Fa4a696EAc66f749f7fe999D6673fEe2026c': {
-      information: {
-        aliasName: 'CHEX',
-        logo: 'https://tokensale.thechex.com/images/chex-token.jpg',
-        website: 'https://tokensale.thechex.com',
-      },
-      events: {
-        Transfer: {
-          args: {
-            tokens: '_value',
-            sender: '_to',
-          },
-          customArgs: {
-            _from: '0x0000000000000000000000000000000000000000',
-          },
-          firstTransactionBlockNumber: 4204457,
-          lastTransactionBlockNumber: 4550057,
-          countTransactions: true,
-        }
-      },
-      icoParameters: {
-        cap: async (web3, icoContract) => {
-          const total = convertWeb3Value(await toPromise(icoContract.tokenCap)(), 'ether');
-          return `Max ${formatNumber(total)} CHX`;
-        },
-        startDate: async icoContract => 'contract creation',
-        endDate: async icoContract => 'not provided',
-        status: async (web3, icoContract) => {
-          const frozen = await toPromise(frozen)().valueOf();
-          return frozen ? 'not provided' : 'in progress';
-        },
-      },
-      matrix: {
-        q1: { answer: true },
-        q2: { answer: true },
-        q3: { answer: true },
-        q4: { answer: true },
-        q5: { answer: true },
-        q6: { answer: true },
-        q7: { answer: true },
-        q8: { answer: true, comment: 'Adjusts to estimate current bitcoin:ethereum prices, and generates a receipt for real time transaction' },
-        q9: { answer: false, comment: 'Non-ethereum transactions (bitcoin, altcoins) are processed primarily through a centralized server' },
-        q10: { answer: true, comment: 'Simplified and uses common practices' },
-        q11: { answer: true, comment: 'The only exception to this is that the contract uses block time to change phases, which can be lead to slightly early/late dates depending on network congestion' },
-        q12: { answer: true, comment: 'Price scales according to current block number while sale is active' },
-        q13: { answer: true, comment: 'Determined by starting block number' },
-        q14: { answer: true, comment: 'Will end either by supply limit reached, manual freeze, or end block reached' },
-      },
-      addedBy: 'Bravetarget',
     },
   },
   rpcHost,

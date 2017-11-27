14dd0b7364a4de0a5eb1e72d5a5f669b9d83d5f4
mostafabalata <mostafa.balata@gmail.com>
2017-11-27 18:00:19 +0100

    Modify the comments

--- src/icos_config/coinlancer.js
+++ src/icos_config/coinlancer.js
@@ -26,20 +26,20 @@ export default {
     status: async () => 'Not Available',
   },
   matrix: {
-    q1: { answer: false, comment: `Funds are collected on backend. Token contract exists
-     but it is not used during fundraiser.` },
+    q1: { answer: false, comment: `Token contract exists and is used by 
+    backend (owner address) to distribute tokens.` },
     q2: { answer: null },
     q3: { answer: false, comment: `Token contract exists, but it's not used to 
     transfer the money in transparent way.` },
     q4: { answer: null },
-    q5: { answer: false, comment: 'Smart contract doesn\'t say any thing.' },
+    q5: { answer: false, comment: `we use standard Transfer function to 
+    display analytics. No ETH information available` },
     q6: { answer: false },
     q7: { answer: false },
     q8: { answer: false, comment: 'All other currencies are handled by a backend.' },
     q9: { answer: false },
-    q10: { answer: false, comment: `There is no real ICO contract, they only 
-    implement basic transfer method.` },
-    q11: { answer: false },
+    q10: { answer: false },
+    q11: { answer: true, comment: `Simple transferable token, no ICO contract available` },
     q12: { answer: null },
     q13: { answer: null },
     q14: { answer: null },

7594cc9eb24afa8e180db7065a382641383db476
thanawatnew <thanawatnew@gmail.com>
2018-02-27 18:53:23 +0700

    Fix eslint errors

--- src/icos_config/devnetwork.js
+++ src/icos_config/devnetwork.js
@@ -1,9 +1,9 @@
 export default {
-  crowdSaleTokenContract:'devnetwork-smart-contract-not-provided',
+  crowdSaleTokenContract: 'devnetwork-smart-contract-not-provided',
   information: {
     name: 'Devnetwork',
     website: 'https://dev.wi.th/ico',
-    logo: 'https://dev.wi.th/ico/wp-content/uploads/2018/01/utility-token.png'
+    logo: 'https://dev.wi.th/ico/wp-content/uploads/2018/01/utility-token.png',
   },
   events: {},
   icoParameters: {
@@ -19,9 +19,9 @@ export default {
     q4: { answer: null, comment: '' },
     q5: { answer: false, comment: '' },
     q6: { answer: false, comment: 'Funds gathering process is not auditable. It happens via an account generated per investor for which it does not have private key.' },
-    q7: { answer: null, comment: '' },
-    q8: { answer: false, comment: 'Internal calculations happen in ETH'  },
-    q9: { answer: null, comment: 'Transactions are processed through an account generated per investor in each coin\'s respective network (except XLM and XRP that use different memos generation).' },
+    q7: { answer: false, comment: '' },
+    q8: { answer: false, comment: 'Internal calculations happen in ETH' },
+    q9: { answer: false, comment: 'Transactions are processed through an account generated per investor in each coin\'s respective network (except XLM and XRP that use different memos generation).' },
     q10: { answer: false, comment: '' },
     q11: { answer: false, comment: 'All stages of ICO are triggered by the admin. Token transfers are enabled when ICO is finalized so it is subject to admin good will and is not controlled by the contract.' },
     q12: { answer: false, comment: '' },
@@ -30,4 +30,4 @@ export default {
   },
   addedBy: 'thanawatnew',
   dateAdded: '27-02-2018',
-}
+};

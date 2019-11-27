341c273051e5330a9168a7adc7b184a24f886137
lexey <tblz@palavara.com>
2019-11-27 14:35:46 +0100

    remove logging

--- src/components/IframeCssTweaker.js
+++ src/components/IframeCssTweaker.js
@@ -18,8 +18,6 @@ export class IframeCssTweaker extends Component {
 
   setBackgroundColor() {
     const root = document.documentElement;
-    body.classList.add('body-iframe-empty')
-    console.log(">>--->> this is an iframe:", this.checkIframe())
     if(this.checkIframe()){
       const body = document.getElementsByTagName('body')[0]
       body.classList.add('body-iframe')
